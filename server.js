4f4c709b5a4f5f9c4f08fbe3cabf3f667e37e742
trendmanishGIT <trendymanish@gmail.com>
2022-11-09 18:24:21 +0530

    morgan running

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 // Load env var
-
 dotenv.config({ path: "./config/config.env" });
-
-const app = express();
 const PORT = process.env.PORT || 4000;
 
+const app = express();
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 const bootcamps = require("./routes/routes.js");
+
+// dev logging middleware - Morgan
 
 app.use("/api/v1/bootcamps", bootcamps);
 
